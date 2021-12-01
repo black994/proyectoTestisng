5126a185aec4d6baee1e40f1f62a10f2a7b9e96b
black994 <jp9496@hotmail.com>
2021-12-01 02:08:55 -0600

    Pruebas proy 5

--- test_pyson.py
+++ test_pyson.py
@@ -6,6 +6,18 @@ from pyson import LeerURL
 #run test default
 def test_campeon():
     assert campeones == 102
+    
+    
+def test_campeon():
+    assert campeones == 19
+
+
+def test_campeon():
+    assert campeones == 245
+
+
+def test_campeon():
+    assert campeones == 157
 
 def test_leerURL():
     assert LeerURL() == "https://la1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/6VwWJ3bgVMf3X7TqZgE0auUVOULT7ybPTUHPSZ0sNXvSShg?api_key=RGAPI-213fa414-3a82-43e0-90df-5d96c1d98233"
