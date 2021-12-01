69789d35354ac0bb3231c949545aa8884c940e07
black994 <jp9496@hotmail.com>
2021-12-01 02:03:05 -0600

    Pruebas proy 3

--- test_pyson.py
+++ test_pyson.py
@@ -8,6 +8,6 @@ def test_campeon():
     assert campeones == 102
 
 def test_leerURL():
-    assert LeerURL() == None
+    assert LeerURL() == "https://la1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/6VwWJ3bgVMf3X7TqZgE0auUVOULT7ybPTUHPSZ0sNXvSShg?api_key=RGAPI-213fa414-3a82-43e0-90df-5d96c1d98233"
 
 
